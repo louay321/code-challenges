2ffe79366845f973fa4e37240a0e2cfa95794fc5
louay nahdi <paladinx@live.fr>
2021-07-02 14:00:27 +0200

    added first file

---
+++ simple/A-square-of-squares.py
@@ -0,0 +1,23 @@
+""""
+A codewars task solution
+
+Task:
+Given an integral number, determine if it's a square number:
+
+In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+"""
+
+# Solution
+import math
+def is_square(n):
+    return ( int(math.sqrt(n)+0.5)**2 == n if n >=0 else False)
