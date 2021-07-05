ad9c8289f8a42a947848627cd6be3f1435d06dc9
Louay <34587388+louay321@users.noreply.github.com>
2021-07-05 19:22:18 +0200

    Create Fibonacci-Tribonacci-and-friends.py

---
+++ medium/Fibonacci-Tribonacci-and-friends.py
@@ -0,0 +1,24 @@
+"""
+--CODEWARS--
+
+TASK :
+
+Well, time to expand the family a little more: think of a Quadribonacci starting with a signature of 4 elements and each following element is the sum of the 4 previous, a Pentabonacci (well Cinquebonacci would probably sound a bit more italian, but it would also sound really awful) with a signature of 5 elements and each following element is the sum of the 5 previous, and so on.
+
+Well, guess what? You have to build a Xbonacci function that takes a signature of X elements - and remember each next element is the sum of the last X elements - and returns the first n elements of the so seeded sequence.
+
+xbonacci {1,1,1,1} 10 = {1,1,1,1,4,7,13,25,49,94}
+xbonacci {0,0,0,0,1} 10 = {0,0,0,0,1,1,2,4,8,16}
+xbonacci {1,0,0,0,0,0,1} 10 = {1,0,0,0,0,0,1,2,3,6}
+xbonacci {1,1} produces the Fibonacci sequence
+"""
+def Xbonacci(signature,n):
+    result = signature[:]
+    #assign result as array equal to signature 
+    for x in range(n-len(signature)): #loop for the rest numbers we want to add to sequence
+        current_fib = 0
+        start = len(result) - len(signature) #new starting point each time
+        for y in result[start:]: #
+            current_fib += y
+        result.append(current_fib)
+    return result[:n]
